ce8d222763dc827934266472cfbe9c04d2312f56
Keegan Williams <KeeganW@users.noreply.github.com>
2022-03-31 23:48:07 -0700

    Adding new icons and using bootstrap.

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,29 @@
 import React from "react"
 import { Outlet, Link } from "react-router-dom"
+import {Button, ButtonToolbar, Container} from "react-bootstrap";
 
 export default function App() {
     return (
-        <div>
-            <Link to=""><h1>Pick Player</h1></Link>
-            <nav
-                style={{
-                    borderBottom: "solid 1px",
-                    paddingBottom: "1rem",
-                }}
-            >
-                <Link to="custom">Custom Lists</Link>
-                <Link to="game">By Game</Link>
-            </nav>
-            <Outlet />
-        </div>
+        <Container className="p-3">
+            <Container className="p-5 mb-4 bg-light rounded-3">
+                <Link to=""><h1>Pick Player</h1></Link>
+                <nav
+                    style={{
+                        borderBottom: "solid 1px",
+                        paddingBottom: "1rem",
+                    }}
+                >
+                    <ButtonToolbar className="custom-btn-toolbar">
+                        <Link to="custom">
+                            <Button>Custom Lists</Button>
+                        </Link>
+                        <Link to="game">
+                            <Button>By Game</Button>
+                        </Link>
+                    </ButtonToolbar>
+                </nav>
+                <Outlet />
+            </Container>
+        </Container>
     );
 }
